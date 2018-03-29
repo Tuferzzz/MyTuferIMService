b344f33e6f9688fb52a44b8d4c6d6181f4589de2
Tufer <1126179195@qq.com>
2018-03-29 16:09:17 +0800

    实现登录注册以及好友关系的接口

--- src/main/java/com/tufer/mylove/web/push/Application.java
+++ src/main/java/com/tufer/mylove/web/push/Application.java
@@ -1,6 +1,8 @@
 package com.tufer.mylove.web.push;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.tufer.mylove.web.push.provider.AuthRequestFilter;
+import com.tufer.mylove.web.push.provider.GsonProvider;
 import com.tufer.mylove.web.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -13,11 +15,16 @@ import java.util.logging.Logger;
 public class Application extends ResourceConfig {
     public Application(){
         // 注册逻辑处理的包名
-        //packages("net.qiujuer.web.italker.push.service");
+        //packages("com.tufer.mylove.web.push");
         packages(AccountService.class.getPackage().getName());
 
+        // 注册我们的全局请求拦截器
+        register(AuthRequestFilter.class);
+
         // 注册Json解析器
-        register(JacksonJsonProvider.class);
+        // register(JacksonJsonProvider.class);
+        // 替换解析器为Gson
+        register(GsonProvider.class);
 
         // 注册日志打印输出
         register(Logger.class);
